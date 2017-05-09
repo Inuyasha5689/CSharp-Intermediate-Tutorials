4e8a72bd8c81dadbb33991b7e3a0773b4ebcd28e
Drake Scott <dascott0204@gmail.com>
2017-05-09 02:08:52 -0500

    Properties in C#.

---
+++ Properties/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public DateTime Birthdate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var TimeSpan = DateTime.Today - Birthdate;
+                var years = TimeSpan.Days / 365;
+
+                return years;
+            }
+
+        }
+    }
+}
