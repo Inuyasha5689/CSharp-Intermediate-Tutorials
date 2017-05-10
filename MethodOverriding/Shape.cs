bdc2b81505e3c424d93c87084fc228713629193b
Drake Scott <dascott0204@gmail.com>
2017-05-10 03:31:27 -0500

    Abstract classes and Members in C#

--- MethodOverriding/Shape.cs
+++ MethodOverriding/Shape.cs
@@ -29,7 +29,7 @@ namespace MethodOverriding
     {
         public int Width { get; set; }
         public int Height { get; set; }
-        public Position Position { get; set; }
+//        public Position Position { get; set; }
 
         public virtual void Draw()
         {
